081c55c2cb223f3843dc302e4e82f586904fd022
Aya <tricera@duck.com>
2025-05-10 20:21:03 +0200

    retrieving annotations via API as cas xmi-xml and storing them in memory now works

--- post_inception_processing.py
+++ post_inception_processing.py
@@ -1,12 +1,42 @@
 from pycaprio import Pycaprio
 from pycaprio.mappings import InceptionFormat
-client = Pycaprio("http://localhost:8080", authentication=("remote-api", "MDT2azx-qae0fbg*uhz"))
+from zipfile import ZipFile
+from io import BytesIO
+from cassis import Cas
 
-# List projects
-projects = client.api.projects()
+INCEPTION_URL = "http://localhost:8080"
+INCEPTION_USER = "remote-api"
+INCEPTION_PW = "MDT2azx-qae0fbg*uhz"
+PROJECT_ID = 2
+INCEPTION_FORMAT = InceptionFormat.UIMA_CAS_XMI_XML_1_1
+USER = 'admin'
 
-for project in projects:
-    print(project.project_id)
+SENTENCE_TYPE = "de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence"
+TOKEN_TYPE = "de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token"
+DEPENDENCY_TYPE = "de.tudarmstadt.ukp.dkpro.core.api.syntax.type.dependency.Dependency"
+LEMMA_TYPE = "de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Lemma"
+POS_TYPE = "de.tudarmstadt.ukp.dkpro.core.api.lexmorph.type.pos.POS"
+MORPH_TYPE = "de.tudarmstadt.ukp.dkpro.core.api.lexmorph.type.morph.MorphologicalFeatures"
+SPAN_TYPE = "custom.Span"
+RELATION_TYPE = "webanno.custom.Relation"
+COREFERENCE_TYPE = "webanno.custom.Coreferencesaslinks"
+NAMED_ENTITY_TYPE = "de.tudarmstadt.ukp.dkpro.core.api.ner.type.NamedEntity"
+CLITORIS_ENTITY_TYPE = "webanno.custom.ClitorisEntities"
+SURFACE_FORM_TYPE = "de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.SurfaceForm"
+
+
+client = Pycaprio(INCEPTION_URL, authentication=(INCEPTION_USER, INCEPTION_PW))
+
+annotation_zip = ZipFile(BytesIO(client.api.annotation(PROJECT_ID, 1, USER, annotation_format=INCEPTION_FORMAT))) # Downloads annotations on document 1
+
+for item in [i for i in annotation_zip.filelist if i.filename.endswith(".xmi")]:
+    annotation_xmi = annotation_zip.read(item)
+    print(len(annotation_xmi))
+
+with open("tmp/test.xmi", 'wb') as f:
+    f.write(annotation_xmi)
+
+print("annotations downloaded")
     
 
 """
@@ -30,3 +60,4 @@ Vorgehen
     - wenn eine referenz auf einen Coreferencesaslinks-span mit ClitRef-Feature im Span liegt -> den Wert übernehmen
 - Für alle pred-Spans: rekursiv Dependent-Spans suchen und dieses Set von Spans als Generalproposition speichern
 - Je Generalproposition die Teilpropositionen berechnen und speichern
+"""
