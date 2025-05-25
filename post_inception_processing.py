d5f8c72c4784f266d85873358a1ec5dd4205f8bf
Aya <tricera@duck.com>
2025-05-26 00:03:36 +0200

    proposition extraction including clitents and corefs works now! (mostly)

--- post_inception_processing.py
+++ post_inception_processing.py
@@ -33,10 +33,18 @@ def key_elemType(propElem):
     elemOrder = ["zus", "narg", "farg", "pspez", "gspez"]
     try:
         keyword = propElem[0].label.split()[0]
-        rank = elemOrder.index(keyword)
     except:
-        rank = 0
-    return propElem[0] is rank
+        keyword = propElem[0][0].label.split()[0]
+    rank = elemOrder.index(keyword)
+    #print(rank)
+    return rank
+
+def key_dictKey(item):
+    key = item[0]
+    keyOrder = ["id", "zus", "lemma", "narg", "farg", "pspez", "gspez"]
+    keyword = list(filter(lambda k: key.startswith(k), keyOrder))[0]
+    rank = keyOrder.index(keyword)
+    return rank
 
 def check_for_sub_elems(cas, span):
     subElems = [r.Dependent for r in cas.select(RELATION_TYPE) if r.Governor.xmiID==span[0].xmiID]
@@ -79,8 +87,7 @@ def check_for_sub_elems(cas, span):
         return struct_subElems_
     else:
         struct_subElems = list(itertools.product(*struct_subElems))
-        print("return:", struct_subElems)
-        #TODO: Evtl dieses Konstrukt nochmal als Annotation speichern?
+        #print("return:", struct_subElems)
         return struct_subElems
 
 def elementString(arg):
@@ -97,19 +104,106 @@ def elementString(arg):
         propArgStr += arg.get_covered_text()
     return propArgStr
 
+def get_elem_text(elem):
+    if type(elem)==tuple:
+        ret = " ".join(map(lambda span: get_surface_text(span), elem))
+    elif type(elem)==list:
+        ret = "\n".join(map(lambda item: get_elem_text(item), elem))
+    else:
+        ret = get_surface_text(elem)
+    return ret
+    
+def get_elem_coref(elem, runType='default'):
+    corefs = []
+    begins = []
+    ends = []
+    if type(elem)==tuple:
+        for span in elem:
+            if span.coreference: 
+                corefs.append((span.coreference.head, span.coreference.begin, span.coreference.end))
+                begins.append(span.begin)
+                ends.append(span.end)
+    elif type(elem)==list:
+        for item in elem:
+            output = get_elem_coref(item, runType='raw')
+            for coref in output[0]: corefs.append(coref)
+            for begin in output[1]: begins.append(begin) 
+            for end in output[2]: ends.append(end)
+    else:
+        if elem.coreference: 
+            corefs.append((elem.coreference.head, elem.coreference.begin, elem.coreference.end))
+            begins.append(elem.begin)
+            ends.append(elem.end)
+            
+    if runType=='default':
+        ret = []
+        for coref in set(corefs):
+            if coref[1] <= min(begins) and coref[2] >= max(ends):
+                fullCoverage = True
+            else:
+                fullCoverage = False
+            ret.append((coref[0], fullCoverage))
+        return ret
+    else:
+        return corefs, begins, ends
+        
+def get_elem_clitents(elem, runType='default'):
+    clitEnts = []
+    begins = []
+    ends = []
+    if type(elem)==tuple:
+        for span in elem:
+            if span.clitEnt:
+                clitEnts.append((span.clitEnt, span.clitEnt.begin, span.clitEnt.end))
+                begins.append(span.begin)
+                ends.append(span.end)
+    elif type(elem)==list:
+        for item in elem:
+            output = get_elem_clitents(item, runType='raw')
+            for clitEnt in output[0]: clitEnts.append(clitEnt)
+            for begin in output[1]: begins.append(begin) 
+            for end in output[2]: ends.append(end)
+    else:
+        if elem.clitEnt: 
+            clitEnts.append((elem.clitEnt, elem.clitEnt.begin, elem.clitEnt.end))
+            begins.append(elem.begin)
+            ends.append(elem.end)
+            
+    if runType=='default':
+        ret = []
+        for clitEnt in set(clitEnts):
+            if clitEnt[1] <= min(begins) and clitEnt[2] >= max(ends):
+                fullCoverage = True
+            else:
+                fullCoverage = False
+            ret.append((clitEnt[0], fullCoverage))
+        return ret
+    else:
+        return clitEnts, begins, ends
+
 def get_surface_text(span):
-    ret = ""
-    if span.SurfaceForm:
-        return span.SurfaceForm
-    
-    
-    
+    if 'SurfaceForm' in span.type._features.keys():
+        if span.SurfaceForm:
+            return span.SurfaceForm
+    if 'label' in span.type._features.keys():
+        if span.label=='pred':
+            return cas.select_covered(LEMMA_TYPE, span)[0].value
+    if span.surfaceForm_ext and span.surfaceForm_ext.begin >= span.begin and span.surfaceForm_ext.end <= span.end:
+        ret = ""
+        if span.begin < span.surfaceForm_ext.begin:
+            ret += Sofa.sofaString[span.begin:span.surfaceForm_ext.begin]
+        ret += span.surfaceForm_ext.value
+        if span.end > span.surfaceForm_ext.end:
+            ret += Sofa.sofaString[span.surfaceForm_ext.end:span.end]
+        return ret
+    else:
+        return span.get_covered_text()
     
 #initializing the client
 client = Pycaprio(INCEPTION_URL, authentication=(INCEPTION_USER, INCEPTION_PW))
 
 #make a request at Inception and put the response doc into a ZipFile
-annotation_zip = ZipFile(BytesIO(client.api.annotation(PROJECT_ID, 101, USER, annotation_format=INCEPTION_FORMAT))) # Downloads annotations on document 1
+annotation_zip = ZipFile(BytesIO(client.api.annotation(PROJECT_ID, 94, USER, annotation_format=INCEPTION_FORMAT))) # Downloads annotations on document 1
 
 #recognise and deflate the zipfile contents
 if annotation_zip.filelist[0].filename.endswith(".xmi"):
@@ -123,6 +217,9 @@ else:
 typesystem = load_typesystem(BytesIO(typesystem_xml))
 ClitEnt = typesystem.create_feature(domainType=SPAN_TYPE, name="clitEnt", rangeType="uima.cas.AnnotationBase")
 SurfaceForm_ext = typesystem.create_feature(domainType=SPAN_TYPE, name="surfaceForm_ext", rangeType="uima.cas.AnnotationBase")
+SurfaceForm_ext1 = typesystem.create_feature(domainType=COREFERENCE_TYPE, name="surfaceForm_ext", rangeType="uima.cas.AnnotationBase")
+SurfaceForm_ext2 = typesystem.create_feature(domainType=CLITORIS_ENTITY_TYPE, name="surfaceForm_ext", rangeType="uima.cas.AnnotationBase")
+SurfaceForm_ext2a = typesystem.create_feature(domainType=NAMED_ENTITY_TYPE, name="surfaceForm_ext", rangeType="uima.cas.AnnotationBase")
 Coreference = typesystem.create_feature(domainType=SPAN_TYPE, name="coreference", rangeType="uima.cas.AnnotationBase")
 Head = typesystem.create_feature(domainType=COREFERENCE_TYPE, name="head", rangeType="uima.cas.AnnotationBase")
 
@@ -141,27 +238,39 @@ for rel in [r for r in cas.select(RELATION_TYPE) if r.RelationzumPrdikat not in 
     #print("new",old_dependent.label)
 
 for ent in cas.select(CLITORIS_ENTITY_TYPE):
-    for span in cas.select_covered(SPAN_TYPE, ent):
+    for span in cas.select_covering(SPAN_TYPE, ent):
         span.set('clitEnt', ent)
         #print(span)
         
 for ent in cas.select(NAMED_ENTITY_TYPE):
-    for span in cas.select_covered(SPAN_TYPE, ent):
+    for span in cas.select_covering(SPAN_TYPE, ent):
         span.set('clitEnt', ent)
         #print(span)
 
 for surfaceForm in cas.select(SURFACE_FORM_TYPE):
-    for span in cas.select_covered(SPAN_TYPE, surfaceForm):
-        span.set('surfaceForm_ext', surfaceForm)
+    for span in cas.select_covering(SPAN_TYPE, surfaceForm):
+        if surfaceForm.begin >= span.begin and surfaceForm.end <= span.end:
+            span.set('surfaceForm_ext', surfaceForm)
+    for coref in cas.select_covering(COREFERENCE_TYPE, surfaceForm):
+        if surfaceForm.begin >= coref.begin and surfaceForm.end <= coref.end:
+            coref.set('surfaceForm_ext', surfaceForm)
         #print(span)
 
 for coref in cas.select(COREFERENCE_TYPE):
     if coref.Referenzen:
-        coref.set("head", coref.Referenzen.elements[0].target)
-        for span in cas.select_covered(SPAN_TYPE, coref):
+        coref.set('head', coref.Referenzen.elements[0].target)
+        for span in cas.select_covering(SPAN_TYPE, coref):
             span.set('coreference', coref)
             #print(span)
 
+#get information on the current document
+textStr = cas.sofa_string
+bookId = cas.get_document_annotation().documentTitle.split('_')[0]
+excerptId = cas.get_document_annotation().documentTitle.split('_')[1]
+page = cas.get_document_annotation().documentTitle.split('_')[2].split('.')[0]
+
+
+#iterate through all predicate heads in the current document / excerpt
 i_pred = 0
 for pred in filter(lambda s: s.label == "pred", cas.select(SPAN_TYPE)):
     i_pred +=1
@@ -169,6 +278,8 @@ for pred in filter(lambda s: s.label == "pred", cas.select(SPAN_TYPE)):
     lemmaSubs = []
     propNargs = []
     propElems = []
+    
+    #sort Dependents of the current predicate head into different lists
     for relation in filter(lambda rel: rel.Governor.xmiID == pred.xmiID, cas.select(RELATION_TYPE)):
         #print("relation:", relation)
         if relation.Dependent.label in ["predAVZ", "predSub"]:
@@ -180,20 +291,20 @@ for pred in filter(lambda s: s.label == "pred", cas.select(SPAN_TYPE)):
         else:
             propElems.append([relation.Dependent])
     
-    #sort those 3 sublists and check for enumerations and other sub-Spans. "zusätze" mustn't have sub-elements, so the last step is skipped for them.
+    #sort those 3 sublists and check for enumerations and other sub-Spans. "zusätze" mustn't have sub-elements, so the last step is skipped for them. -> untrue, they can't have enums, but they can have subArgs -> TODO!!!
     propZuss.sort(key=lambda s: s[0].begin)
     lemmaSubs.sort(key=lambda s: s[0].begin)
     lemmaSubs = [check_for_sub_elems(cas, s) for s in lemmaSubs]
     propNargs.sort(key=lambda s: s[0].begin)
     propNargs = [check_for_sub_elems(cas, s) for s in propNargs]
-    propElems.sort(key=key_elemType)
     propElems = [check_for_sub_elems(cas, s) for s in propElems]
+    propElems.sort(key=key_elemType)
     
     subLemmata = list(itertools.product(*lemmaSubs))
-    #print("lemmaSubs:", lemmaSubs)
-    print("LemmaProducts:", len(list(itertools.product(*lemmaSubs))))
-    print("PropNargsProducts:", len(list(itertools.product(*propNargs))))
-    print("PropElems:", len(propElems))
+
+    #print("LemmaProducts:", len(list(itertools.product(*lemmaSubs))))
+    #print("PropNargsProducts:", len(list(itertools.product(*propNargs))))
+    #print("PropElems:", len(propElems))
     
     partProps = []
     c_lemma=ord('A')
@@ -211,35 +322,122 @@ for pred in filter(lambda s: s.label == "pred", cas.select(SPAN_TYPE)):
         for nargs in itertools.product(*propNargs):
             i=1
             for narg in nargs:
+                i_ = i if len(nargs)>1 else ""
                 c_ = chr(c) if narg.label.endswith("Elem") else ""
-                prop[f"narg{i}{c_}"] = narg
+                prop[f"narg{i_}{c_}"] = narg
                 i+=1
             partProps.append(prop.copy())
             c_nargs+=1
             
-            i=1
             for elem in propElems:
+                #sameTypeElems = list(filter(lambda e: e[0].label.split()[0]==elem[0].label.split()[0], propElems))
+                #i_ = sameTypeElems.index(elem)+1 if len(sameTypeElems)>1 else ""
+                i_ = ""
                 if len(elem) > 1:
                     c_elems=ord('A')
                     for enum in elem:
-                        type_ = enum.label.split()[0]
-                        prop[f"{type_}{i}{chr(c_elems)}"] = enum
+                        try:
+                            type_ = enum.label.split()[0]
+                        except:
+                            type_ = enum[0].label.split()[0]
+                        prop[f"{type_}{i_}{chr(c_elems)}"] = enum
                         partProps.append(prop.copy())
                         prop.popitem()
                         c_elems+=1
-                    prop[f"{type_}{i}"] = elem
+                    prop[f"{type_}{i_}"] = elem
                 else:
                     type_ = elem[0].label.split()[0]
-                    prop[f"{type_}{i}"] = elem[0]
+                    prop[f"{type_}{i_}"] = elem[0]
                     partProps.append(prop.copy())
-                i+=1
     
-    #add IDs to the partial propositions
+    #add additional information to each proposition
     c_prop=ord('A')
     for prop in partProps:
-        prop["id"] = f"{ chr(i_pred).zfill(2) }{ chr(c_prop) }"
-        c_prop+=1
+        
+        #collect corefs + clitEnts for all elements
+        propCorefs = []
+        propClitEnts = []
+        for key, value in prop.items():
+            
+            i=1
+            corefs = get_elem_coref(value)
+            for coref in corefs:
+                i_= i if len(corefs)>1 else ''
+                newKey = key + f"_coref{i_}"
+                if coref[1]==False: newKey+="*"
+                propCorefs.append((newKey, coref[0]))
+                i+=1
+            
+            i=1
+            clitEnts = get_elem_clitents(value)
+            for ent in clitEnts:
+                i_= i if len(clitEnts)>1 else ''
+                newKey = key + f"_clitEnt{i_}"
+                if ent[1]==False: newKey+="*"
+                propClitEnts.append((newKey, ent[0]))
+                i+=1
+        
+        #add collected corefs and clitEnts to the prop dict
+        for (k,v) in propCorefs:
+            prop[k] = v
+        for (k,v) in propClitEnts:
+            prop[k] = v    
     
+        #add IDs to the partial propositions
+        prop["id"] = f"{ str(i_pred).zfill(2) }{ chr(c_prop) }"
+        c_prop+=1
+
+    
+    #create a seperate dictionary for the string values of each proposition element
+    partPropStrs = partProps.copy() #the new dictionary doesn't have strings in it yet!
+    for prop in partPropStrs:
+        
+        #get strings for all prop entries except "id"
+        lemmaSubs = False
+        for key, value in prop.items():
+            if key.find('_coref')>0: #for coref entries
+                valueStr = get_surface_text(value)
+                prop[key] = valueStr
+                if key.find('*')>0: 
+                    valueStr += ' ("'
+                    valueStr += get_surface_text(value)
+                    valueStr += '")'
+            elif key.find('_clitEnt')>0: #for clitEnt entries
+                valueStr = value.Struktur
+                if value.Bezug:
+                    valueStr += "; -> "
+                    valueStr += str(value.Bezug)
+                if key.find('*')>0: 
+                    valueStr += ' ("'
+                    valueStr += get_surface_text(value)
+                    valueStr += '")'
+                prop[key] = valueStr
+            elif key!="id":                     #for 'normal' entries
+                valueStr = get_elem_text(value)
+                if key.startswith("lemmaSubs"):
+                    lemmaSubs = key
+                    if len(value)>0 and value[-1].label!="predAVZ": valueStr+=" "
+                prop[key] = valueStr
+        
+        
+        #combine lemmaSubs and lemmaHead into lemma 
+        if lemmaSubs:
+            new_key = 'lemma' + lemmaSubs.split('lemmaSubs')[1]
+            prop[new_key] = prop[lemmaSubs] + prop['lemmaHead']
+            prop.pop(lemmaSubs)
+        else:
+            prop['lemma'] = prop['lemmaHead']
+        prop.pop('lemmaHead')
+        
+        sortedProp = dict(sorted(list(prop.items()), key=key_dictKey))
+        prop.clear()
+        prop = sortedProp.copy()
+        
+        for key, value in prop.items():
+            print(key, "\t->\t", value)
+        print("\n")
+        
+        
 """    
     ########
     propZusStrs = []
