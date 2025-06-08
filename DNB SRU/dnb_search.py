87e6724d2106c768ae92599b00f963c06a283821
Aya <tricera@duck.com>
2025-06-08 14:09:19 +0200

    added file dnb_search.py which was forgotten earlier to commit. Contains searchterms for the dnb sru search

---
+++ DNB SRU/dnb_search.py
@@ -0,0 +1,64 @@
+    #klitoris-synonyme
+SEARCHTERMS = ['bulboklitoralorgan*',
+			'clit',
+			'clitor*',
+			'kitzel',
+			'kitzler*',
+			'klit',
+			'klitor*',
+			#'lustknospe*',                -> 0 Treffer
+			'lustorgan*',
+			#'lustperle*',                 -> 0 Treffer
+			'penis muliebre',
+					
+    #vulva-synonyme
+			'cunnus',
+			'fotze*',
+			'foz',
+			'fud',
+			'fut',
+			'intimbereich*',
+			#'intimgegend*',               -> 0 Treffer
+			'intimzone*',
+			'miss brown',
+			'möse*',
+			'pubes*',
+			'pudendum muliebre',
+			'punzel*',
+			'pussi*',
+			'pussy*',
+			'schamgegend*',
+			'scheidenvorhof*',
+			'vestibulum',
+			'vorhof d* scheide*',
+			'vorhof d* vagina*',
+			'votze*',
+			'vulv*',
+			'weiblich* scham*',
+			'yoni*',
+
+    #geschlechtsorgan-synonyme
+            'genital*',
+			'begattungsorgan*',
+			'fortpflanzungsapparat*',
+			'fortpflanzungsorgan*',
+			'fruchtbarkeitsorgan*',
+			'gemächt*',
+			'generationsorgan*',
+			'geschlechtsapparat*',
+			'geschlechtsorgan*',
+			'geschlechtsteil*',
+			'geschlechtswerkzeug*',
+            #'intimorgan*',                 -> 0 Treffer
+			'liebesorgan*',
+			'primär* geschlechtsmerkmal*',
+			'pudendum',
+			'reproduktionsorgan*',
+			'schamteil*',
+			'sexualapparat*',
+			'sexualorgan*',
+			'wollustorgan*',
+            #'wolllustorgan*',              -> 0 Treffer
+			'zeugungsorgan*']
+                
+SEARCHSTRING = '(' + ' or '.join([('"' + s + '"') for s in SEARCHTERMS]) + ')'
