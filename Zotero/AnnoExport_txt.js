f1803b0194d60781e890cc08ee5d4276fed6c830
Aya <tricera@duck.com>
2025-09-26 19:11:35 +0200

    image annotation rectangle export script; used to extract on 26.09.2025

--- Zotero/AnnoExport_txt.js
+++ Zotero/AnnoExport_txt.js
@@ -1,14 +1,29 @@
+async function saveAnnotation(item, annotation, data){
+    let bookID = item["parentItem"].getField("key");
+    let bookTitle = item["parentItem"].getField("title");
+    let year = item["parentItem"].getField("date");
+    let attID = item.getField("key");
+    let attFilename = item.attachmentFilename;
+    var annoID = annotation.key;
+    var annoPage = annotation.annotationPageLabel;
+    var annoRect = annotation.annotationPosition;
+    var title = `${bookID}_${annoID}_${annoPage}`;
+    //var path = `C:\\Users\\annik\\ZoteroExports\\Images\\${bookID}\\${title}.jpg`;
+    let pdfPath = `C:\\Users\\annik\\Zotero\\storage\\${attID}\\${attFilename}`;
+    data.push([bookID, bookTitle, year, annoID, annoPage, pdfPath, annoRect, attFilename]);
+    //await Zotero.File.putContentsAsync(path, annoImage);
+}
+
 var items = Zotero.getActiveZoteroPane().getSelectedItems();
+let data = new Array();
+
 
 for (let item of items) {
-        let allAnnotations = await item.getAnnotations().filter((item) => item.annotationType === "highlight");
+    let allAnnotations = await item.getAnnotations().filter((item) => item.annotationType === "image" && item.annotationColor === "#ffd400"); //yellow
     for (let annotation of allAnnotations) {
-        var bookID = item.key;
-        var annoID = annotation.key;
-        var annoPage = annotation.annotationPageLabel;
-        var annoText = annotation.annotationText;
-        var title = `${bookID}_${annoID}_${annoPage}`;
-        var path = `C:\\Users\\annik\\ZoteroExports\\${bookID}\\${title}.txt`;
-        await Zotero.File.putContentsAsync(path, annoText);
+        saveAnnotation(item, annotation, data);
     }
 }
+const content = data.map(row => row.join('\t')).join('\n');
+const tablePath = `C:\\Users\\annik\\ZoteroExports\\Table_imageAnnotations.csv`;
+await Zotero.File.putContentsAsync(tablePath, content);
